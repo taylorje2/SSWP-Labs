2240b4de1449bd6c8ba627fbc5f1bcf40fdd0933
Jayden <taylorje2@etsu.edu>
2026-04-12 19:35:51 -0400

    Set up for Test Methods
    Organized the tests

--- Lab6Tests/Test1.cs
+++ Lab6Tests/Test1.cs
@@ -7,10 +7,108 @@ namespace Lab6Tests
     [TestClass]
     public sealed class Test1
     {
+        /// <summary>
+        /// Test method for ReadBooks
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestReadBooks()
         {
+            // Arrange
 
+            // Act
+
+            // Assert
+        }
+
+        /// <summary>
+        /// Test method for ReadUsers
+        /// </summary>
+        [TestMethod]
+        public void TestReadUsers()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        /// <summary>
+        /// Test method for AddBook
+        /// </summary>
+        [TestMethod]
+        public void TestAddBook()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        /// <summary>
+        /// Test method for AddUser
+        /// </summary>
+        [TestMethod]
+        public void TestAddUser()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        /// <summary>
+        /// Test method for EditBook
+        /// </summary>
+        [TestMethod]
+        public void TestEditBook()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        /// <summary>
+        /// Test method for EditUser
+        /// </summary>
+        [TestMethod]
+        public void TestEditUser()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        /// <summary>
+        /// Test method for DeleteBook
+        /// </summary>
+        [TestMethod]
+        public void TestDeleteBook()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        /// <summary>
+        /// Test method for DeleteUser
+        /// </summary>
+        [TestMethod]
+        public void TestDeleteUser()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
         }
     }
 }
